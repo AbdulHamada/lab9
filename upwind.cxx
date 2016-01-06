4d033a001857f9a61424fb0db8a9256a997b354c
Abdul Malek Hamada <ABDUL.HAMADA@hhu.de>
2016-01-06 17:49:02 +0100

    Update upwind.cxx

--- upwind.cxx
+++ upwind.cxx
@@ -10,22 +10,24 @@ void writeToFile(const double* const u, const string s, const double dx,
                  const double xmin, const int N);
 void initialize(double* const u, const double dx, const double xmin,
                 const int N);
+void step(double *u1, double *u0, const double dx, const double dt, const int N);
 //---------------------------------------
 int main(){
 
-  const double tEnd = ;
-  const double V = ;
+  const double tEnd = 5.;
+  const double V = 1;
 
-  const int N  = ;
+  const int N  = 256;
   const double xmin = -10;
   const double xmax =  10;
   const double dx = (xmax-xmin)/(N-1);
-  double dt = ;
+  const double dt = dx*2;
   const int Na = 10; // Number of output files up to tEnd
   const int Nk = int(tEnd/Na/dt);
 
   double* u0 = new double[N];
   double* u1 = new double[N];
+  double add;
   double* h;
 
   stringstream strm;
@@ -37,11 +39,16 @@ int main(){
   for(int i=1; i<=Na; i++)
   {
    for(int j=0; j<Nk; j++){
-
+  
+     
       // Put call to step function here
-
+      step(u1, u0,  dx, dt,  N);
+      h=u0;
+      u0=u1;
+      u1=h;
       // swap arrays u0 <-> u1,
       // however do not copy values, be more clever ;)
+      
    }
    strm.str("");
    strm << "u_" << i;
@@ -79,3 +86,21 @@ void writeToFile(const double* const u, const string s, const double dx,
    }
    out.close();
 }
+
+void step(double *u1, double *u0, const double dx, const double dt, const int N)
+{
+    double v=1;
+    for(int i=1; i<N; i++)
+    {
+      u1[i]=u0[i]-v*(dt/dx)*(u0[i]-u0[i-1]);
+      //u1[i]=-v*dt*(u0[i+1]-u0[i-1])/(2*dx)+u0[i];
+    }
+}
+
+/*
+ plot for [i=0:10] 'u_'.i.'' w p
+ plot for [i=0:10] 'u_'.i.'' w 
+ plot 'u_0' w l, 'u_10' w l
+dt klein : dämpfung
+dt groß : explodiert
+ */
